38643bdab0baf865da1539ca61d5a73f572b46c0
Joseph Alves <databentpx@gmail.com>
2015-02-02 15:23:06 -0500

    Solution

---
+++ solution.js
@@ -0,0 +1,41 @@
+var EventEmitter = function () {
+    this.events = {};
+};
+
+EventEmitter.prototype.on = function (eventStr, fn) {
+
+    if (!this.events[eventStr]) {
+        this.events[eventStr] = [];
+    }
+
+    this.events[eventStr].push(fn);
+
+};
+
+EventEmitter.prototype.emit = function (eventStr, eventData) {
+
+    if (!this.events[eventStr]) {
+        return;
+    }
+
+    this.events[eventStr].forEach(function (fn) {
+        fn(eventData);
+    });
+
+};
+
+EventEmitter.prototype.removeListener = function (eventStr, fn) {
+
+    var eventFns = this.events[eventStr];
+
+    if (!eventFns) return;
+
+    var i = eventFns.indexOf(fn);
+
+    if (i === -1) return;
+
+    eventFns.splice(i, 1);
+
+};
+
+module.exports = EventEmitter;
